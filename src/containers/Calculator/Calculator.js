54afccc4c59d25f1a1c546f07e7c78beb91c9a7c
Baktiyar Ishenov <b1963826@gmail.com>
2020-01-07 17:29:16 +0600

    display is done

--- src/containers/Calculator/Calculator.js
+++ src/containers/Calculator/Calculator.js
@@ -1,33 +1,39 @@
 import React, {Component} from 'react';
-import Display from "../../components/Display/Display";
 import './Calculator.css'
 import Button from "../../components/UI/Button/Button";
 import {connect} from "react-redux";
 
 class Calculator extends Component {
 	render() {
+		let bgColor = '#c62828';
+		if (!this.props.isLocked) {
+			bgColor = '#2e7d32';
+		}
 		return (
 			<div className="container">
-				<Display />
-				<div className='calculator-inputs-row'>
-					<Button value={7} />
-					<Button value={8} />
-					<Button value={9} />
-				</div>
-				<div className='calculator-inputs-row'>
-					<Button value={4} />
-					<Button value={5} />
-					<Button value={6} />
-				</div>
-				<div className='calculator-inputs-row'>
-					<Button value={1} />
-					<Button value={2} />
-					<Button value={3} />
-				</div>
-				<div className='calculator-inputs-row'>
-					<Button value="C" />
-					<Button value={0} additionalClass="zero" />
-					<Button value="E" />
+				<h5>{this.props.isLocked ? 'Access Denied' : 'Access Granted'}</h5>
+				<input className="calc-display" type="password" value={this.props.code} disabled />
+				<div className="buttons-block">
+					<div className='calculator-inputs-row'>
+						<Button onClick={() => this.props.addSymbol(7)} value={7} />
+						<Button onClick={() => this.props.addSymbol(8)} value={8} />
+						<Button onClick={() => this.props.addSymbol(9)} value={9} />
+					</div>
+					<div className='calculator-inputs-row'>
+						<Button onClick={() => this.props.addSymbol(4)} value={4} />
+						<Button onClick={() => this.props.addSymbol(5)} value={5} />
+						<Button onClick={() => this.props.addSymbol(6)} value={6} />
+					</div>
+					<div className='calculator-inputs-row'>
+						<Button onClick={() => this.props.addSymbol(1)} value={1} />
+						<Button onClick={() => this.props.addSymbol(2)} value={2} />
+						<Button onClick={() => this.props.addSymbol(3)} value={3} />
+					</div>
+					<div className='calculator-inputs-row'>
+						<Button onClick={this.props.removeSymbol} value={"C"} />
+						<Button onClick={() => this.props.addSymbol(0)} value={0} />
+						<Button onClick={this.props.check} value={"E"} />
+					</div>
 				</div>
 			</div>
 		);
@@ -36,14 +42,17 @@ class Calculator extends Component {
 
 const mapStateToProps = state => {
 	return {
-		counter: state.counter
-	};
+		code: state.code,
+		isLocked: state.isLocked
+	}
 };
 
 const mapDispatchToProps = dispatch => {
 	return {
-		increaseCounter: () => dispatch({type: 'INCREMENT'})
-	};
+		addSymbol:(symbol) => dispatch({type: 'ADD_SYMBOL', symbol: symbol}),
+		removeSymbol:() => dispatch({type: 'REMOVE_SYMBOL'}),
+		check:() => dispatch({type: 'CHECK_PASSCODE'})
+	}
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Calculator);
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,11 +1,36 @@
 const initialState = {
-	correctNum: 0
+	code: '',
+	isLocked: true
 };
+
+const passcode ='1234';
 
 const reducer = (state = initialState, action) => {
-	return state;
+	if (action.type === 'REMOVE_SYMBOL') {
+		return {
+			...state,
+			isLocked: true,
+			code: state.code.substr(0, state.code.length -1)
+		};
+	}
+
+	if (action.type === 'CHECK_PASSCODE') {
+		if (passcode === state.code) {
+			return {
+				...state,
+				isLocked: false
+			}
+		}
+	}
+
+	if (state.code.length < 4 && action.type === 'ADD_SYMBOL') {
+		return {
+			...state,
+			code: state.code + action.symbol
+		};
+	} else {
+		return state;
+	}
 };
-
-
 
 export default reducer;
